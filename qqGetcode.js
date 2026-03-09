29d7e4fdf0df77ef93aa9a378847baaceb7879d8
Diane-Birch <144074233+Diane-Birch@users.noreply.github.com>
2026-03-09 15:16:05 +0800

    Update URL in qqGetcode.js for script response

--- qqGetcode.js
+++ qqGetcode.js
@@ -1,1 +1,1 @@
-^https?://gate-obt\.nqf\.qq\.com/prod/ws\? url script-response-body https://raw.githubusercontent.com/Diane-Birch/card/main/qqfarm-getcode.js
+^https?://gate-obt\.nqf\.qq\.com/prod/ws\? url script-response-body https://raw.githubusercontent.com/Diane-Birch/qx-qq-getcode/main/qqfarm-getcode.js
